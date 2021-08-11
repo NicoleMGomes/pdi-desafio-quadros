7af4441ee34200c148cbcb2b72e15bb7c2175f29
unknown <nicolem.gomes18@gmail.com>
2021-08-11 20:47:14 -0300

    Altera url de api

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    `${process.env.VERCEL_URL}/api` ||
+    'http://localhost:3000/api',
 })
