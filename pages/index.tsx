5925fa0777fc99c097ac209c8e759aac030cedea
unknown <nicolem.gomes18@gmail.com>
2021-08-11 22:12:31 -0300

    Ajustando base

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import styled from 'styled-components'
-import { IExample } from '../@types/example.types'
 import ComponentExample from '../components/ComponentExample'
-import { api } from '../services/api'
 
 const Title = styled.h1`
   color: var(--secondary);
@@ -11,25 +8,15 @@ const Title = styled.h1`
   font-size: 50px;
 `
 
-const IndexPage: React.FC<IExample> = ({ stringFromServer, name }) => (
+const IndexPage: React.FC = () => (
   <main>
     <Head>
       <title>Template Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Title>{stringFromServer}</Title>
-    <ComponentExample name={name} />
+    <Title>PDI Desafio dos Quadros</Title>
+    <ComponentExample name="Test" />
   </main>
 )
 
 export default IndexPage
-
-export const getStaticProps: GetStaticProps = async () => {
-  const { data: dataFromServer } = await api.get<IExample>('/example')
-
-  return {
-    props: {
-      ...dataFromServer,
-    },
-  }
-}
--- services/api.ts
+++ services/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    `${process.env.VERCEL_URL}/api` ||
-    'http://localhost:3000/api',
+    process.env.VERCEL_URL ||
+    'http://localhost:3000',
 })
